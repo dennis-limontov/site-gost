a7a66fe618e0f3da800896e1d79d05f9ec14599d
dennis.limontov <dennis.limontov@gmail.com>
2020-07-10 22:01:43 +0300

    added anchors with scrolling

---
+++ src/app/menu/services/goto.js
@@ -0,0 +1,9 @@
+var app = angular.module('angularjs-starter', []);
+
+app.controller('MainCtrl', function($scope, $location, $anchorScroll) {
+    $scope.scrollTo = function(id) {
+        $location.hash(id);
+        console.log($location.hash());
+        $anchorScroll();
+    };
+});
--- src/app/menu/services/services.component.ts
+++ src/app/menu/services/services.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
+import "./goto.js";
 
 @Component({
   selector: 'app-services',
   templateUrl: './services.component.html',
   styleUrls: ['./services.component.css']
 })
-export class ServicesComponent implements OnInit {
+export class ServicesComponent implements OnInit, AfterViewInit {
+  @ViewChild('services_or') serv;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  ngAfterViewInit() {
+    this.focus();
+  }
+
+  focus() {
+    // this.serv.nativeElement.scrollIntoView();
   }
 
 }
