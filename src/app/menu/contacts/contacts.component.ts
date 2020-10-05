066c25e75949c30bb140cf872dec0bcebe7e691e
dennis.limontov <dennis.limontov@gmail.com>
2020-10-05 05:18:59 +0300

    redesigned goods and contacts

--- src/app/menu/contacts/contacts.component.ts
+++ src/app/menu/contacts/contacts.component.ts
@@ -15,4 +15,12 @@ export class ContactsComponent implements OnInit {
     maps();
   }
 
+  showCurrentContact(ev) : void {
+    var index = Array.from(ev.target.parentElement.children).indexOf(ev.target);
+    var contacts = document.getElementsByClassName("contact");
+    for (var i = 0; i < contacts.length; i++) {
+      contacts[i].classList.add("hidden");
+    }
+    contacts[index].classList.remove("hidden");
+  }
 }
