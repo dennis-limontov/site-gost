731d9315015a3f129f7b131597b932adef46668c
dennis.limontov <dennis.limontov@gmail.com>
2020-10-08 01:36:17 +0300

    contacts: added full info

--- src/app/menu/contacts/contacts.component.ts
+++ src/app/menu/contacts/contacts.component.ts
@@ -18,9 +18,18 @@ export class ContactsComponent implements OnInit {
   showCurrentContact(ev) : void {
     var index = Array.from(ev.target.parentElement.children).indexOf(ev.target);
     var contacts = document.getElementsByClassName("contact");
+    var contactNames = document.getElementsByClassName("contact-name");
+    var buttons = document.querySelectorAll(".buttons button");
+    var contactList = document.getElementsByClassName("contact-list");
     for (var i = 0; i < contacts.length; i++) {
       contacts[i].classList.add("hidden");
+      contactNames[i].classList.add("hidden");
+      buttons[i].classList.remove("current");
+      contactList[i].classList.add("hidden");
     }
     contacts[index].classList.remove("hidden");
+    contactNames[index].classList.remove("hidden");
+    contactList[index].classList.remove("hidden");
+    ev.target.classList.add("current");
   }
 }
