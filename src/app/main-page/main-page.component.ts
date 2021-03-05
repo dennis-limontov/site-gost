11d830bc85ce3c13f5e9a9d6276781d8d1d96df6
dennis.limontov <dennis.limontov@gmail.com>
2021-03-05 16:15:07 +0300

    changed meta content; compressed all images

--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -21,74 +21,74 @@ export class MainPageComponent implements OnInit {
       title: 'Ветка',
       description: 'Жилой дом, 30 квартир',
       buttonText: 'Увеличить',
-      img: '../../../assets/img/houses/residential/house-res-001.jpg'
+      img: '../../../assets/img/houses/residential/house-res-001-min.jpg'
     },
     {
       title: 'Житковичи',
       description: 'Жилой дом, 40 квартир',
       buttonText: 'Увеличить',
-      img: '../../../assets/img/houses/residential/house-res-002.jpg',
+      img: '../../../assets/img/houses/residential/house-res-002-min.jpg',
       class: 'minimized'
     },
     {
       title: 'Калинковичи',
       description: 'Жилой дом, 45 квартир',
       buttonText: 'Увеличить',
-      img: '../../../assets/img/houses/residential/house-res-003.jpg'
+      img: '../../../assets/img/houses/residential/house-res-003-min.jpg'
     },
     {
       title: 'Киров',
       description: 'Общежитие',
       buttonText: 'Увеличить',
-      img: '../../../assets/img/houses/residential/house-res-004.jpg'
+      img: '../../../assets/img/houses/residential/house-res-004-min.jpg'
     },
     {
       title: 'Петриков',
       description: 'Жилой дом',
       buttonText: 'Увеличить',
-      img: '../../../assets/img/houses/residential/house-res-005.jpg'
+      img: '../../../assets/img/houses/residential/house-res-005-min.jpg'
     },
     {
       title: 'Ровковичи',
       description: 'Одноквартирные жилые дома',
       buttonText: 'Увеличить',
-      img: '../../../assets/img/houses/residential/house-res-006.jpg'
+      img: '../../../assets/img/houses/residential/house-res-006-min.jpg'
     },
     {
       title: 'Чечерск',
       description: 'Жилой дом, 40 квартир',
       buttonText: 'Увеличить',
-      img: '../../../assets/img/houses/residential/house-res-007.jpg'
+      img: '../../../assets/img/houses/residential/house-res-007-min.jpg'
     },
     {
       title: 'Житковичи',
       description: 'Жилой дом, 60 квартир, 10-й мкр-н',
       buttonText: 'Увеличить',
-      img: '../../../assets/img/houses/residential/house-res-008.jpg'
+      img: '../../../assets/img/houses/residential/house-res-008-min.jpg'
     },
     {
       title: 'Житковичи',
       description: 'Жилой дом, 40 квартир, мкр-н Озёрный',
       buttonText: 'Увеличить',
-      img: '../../../assets/img/houses/residential/house-res-009.jpg'
+      img: '../../../assets/img/houses/residential/house-res-009-min.jpg'
     },
     {
       title: 'Ветка',
       description: 'Жилой дом, 60 квартир',
       buttonText: 'Увеличить',
-      img: '../../../assets/img/houses/residential/house-res-010.jpg'
+      img: '../../../assets/img/houses/residential/house-res-010-min.jpg'
     },
     {
       title: 'Ветка',
       description: 'Жилой дом',
       buttonText: 'Увеличить',
-      img: '../../../assets/img/houses/residential/house-res-011.jpg'
+      img: '../../../assets/img/houses/residential/house-res-011-min.jpg'
     },
     {
       title: 'Петриков',
       description: 'Жилой дом',
       buttonText: 'Увеличить',
-      img: '../../../assets/img/houses/residential/house-res-012.JPG'
+      img: '../../../assets/img/houses/residential/house-res-012-min.JPG'
     },
 
     // agricultural
@@ -96,49 +96,49 @@ export class MainPageComponent implements OnInit {
       title: 'Житковичи',
       description: 'Котельная',
       buttonText: 'Увеличить',
-      img: '../../../assets/img/houses/agricultural/house-agr-001.jpg'
+      img: '../../../assets/img/houses/agricultural/house-agr-001-min.jpg'
     },
     {
       title: 'Добрушский р-н',
       description: 'Котельные',
       buttonText: 'Увеличить',
-      img: '../../../assets/img/houses/agricultural/house-agr-002.jpg'
+      img: '../../../assets/img/houses/agricultural/house-agr-002-min.jpg'
     },
     {
       title: 'Гомель',
       description: 'Завод "Кристалл"',
       buttonText: 'Увеличить',
-      img: '../../../assets/img/houses/agricultural/house-agr-003.jpg'
+      img: '../../../assets/img/houses/agricultural/house-agr-003-min.jpg'
     },
     {
       title: 'МТФ',
       description: 'МТФ',
       buttonText: 'Увеличить',
-      img: '../../../assets/img/houses/agricultural/house-agr-004.jpg'
+      img: '../../../assets/img/houses/agricultural/house-agr-004-min.jpg'
     },
     {
       title: 'МТФ',
       description: 'МТФ',
       buttonText: 'Увеличить',
-      img: '../../../assets/img/houses/agricultural/house-agr-005.png'
+      img: '../../../assets/img/houses/agricultural/house-agr-005-min.png'
     },
     {
       title: 'Песочная Буда',
       description: 'Птицефабрика "Рассвет"',
       buttonText: 'Увеличить',
-      img: '../../../assets/img/houses/agricultural/house-agr-006.png'
+      img: '../../../assets/img/houses/agricultural/house-agr-006-min.png'
     },
     {
       title: 'Ровковичи',
       description: 'Свиноводческий комплекс',
       buttonText: 'Увеличить',
-      img: '../../../assets/img/houses/agricultural/house-agr-007.jpg'
+      img: '../../../assets/img/houses/agricultural/house-agr-007-min.jpg'
     },
     {
       title: 'Хальч',
       description: 'ОАО "Хальч"',
       buttonText: 'Увеличить',
-      img: '../../../assets/img/houses/agricultural/house-agr-008.jpg'
+      img: '../../../assets/img/houses/agricultural/house-agr-008-min.jpg'
     },
     
     // social
@@ -146,91 +146,91 @@ export class MainPageComponent implements OnInit {
       title: 'Ветка',
       description: 'Бассейн',
       buttonText: 'Увеличить',
-      img: '../../../assets/img/houses/social/house-soc-001.jpg'
+      img: '../../../assets/img/houses/social/house-soc-001-min.jpg'
     },
     {
       title: 'Житковичи',
       description: 'Больница',
       buttonText: 'Увеличить',
-      img: '../../../assets/img/houses/social/house-soc-002.jpg'
+      img: '../../../assets/img/houses/social/house-soc-002-min.jpg'
     },
     {
       title: 'Житковичи',
       description: 'Гостиница "Четверть Века"',
       buttonText: 'Увеличить',
-      img: '../../../assets/img/houses/social/house-soc-003.jpg'
+      img: '../../../assets/img/houses/social/house-soc-003-min.jpg'
     },
     {
       title: 'Добруш',
       description: '',
       buttonText: 'Увеличить',
-      img: '../../../assets/img/houses/social/house-soc-004.jpg'
+      img: '../../../assets/img/houses/social/house-soc-004-min.jpg'
     },
     {
       title: 'Петриков',
       description: 'Дом культуры',
       buttonText: 'Увеличить',
-      img: '../../../assets/img/houses/social/house-soc-005.jpg'
+      img: '../../../assets/img/houses/social/house-soc-005-min.jpg'
     },
     {
       title: 'Радуга',
       description: 'ГУО "Радужский детский сад"',
       buttonText: 'Увеличить',
-      img: '../../../assets/img/houses/social/house-soc-006.jpg'
+      img: '../../../assets/img/houses/social/house-soc-006-min.jpg'
     },
     {
       title: 'Чечерск',
       description: 'Прокуратура',
       buttonText: 'Увеличить',
-      img: '../../../assets/img/houses/social/house-soc-007.jpg'
+      img: '../../../assets/img/houses/social/house-soc-007-min.jpg'
     },
     {
       title: 'Районная ЦРБ',
       description: 'Районная ЦРБ',
       buttonText: 'Увеличить',
-      img: '../../../assets/img/houses/social/house-soc-008.jpg'
+      img: '../../../assets/img/houses/social/house-soc-008-min.jpg'
     },
     {
       title: 'Ветка',
       description: 'Детский сад',
       buttonText: 'Увеличить',
-      img: '../../../assets/img/houses/social/house-soc-009.JPG'
+      img: '../../../assets/img/houses/social/house-soc-009-min.JPG'
     },
     {
       title: 'Чечерск',
       description: 'СОК "Бриз"',
       buttonText: 'Увеличить',
-      img: '../../../assets/img/houses/social/house-soc-010.jpg'
+      img: '../../../assets/img/houses/social/house-soc-010-min.jpg'
     },
     {
       title: 'Петриковский р-н',
       description: 'Средняя школа',
       buttonText: 'Увеличить',
-      img: '../../../assets/img/houses/social/house-soc-011.jpg'
+      img: '../../../assets/img/houses/social/house-soc-011-min.jpg'
     },
     {
       title: 'Ветка',
       description: 'ФОК',
       buttonText: 'Увеличить',
-      img: '../../../assets/img/houses/social/house-soc-012.jpg'
+      img: '../../../assets/img/houses/social/house-soc-012-min.jpg'
     },
     {
       title: 'Житковичи',
       description: 'ФОК',
       buttonText: 'Увеличить',
-      img: '../../../assets/img/houses/social/house-soc-013.jpg'
+      img: '../../../assets/img/houses/social/house-soc-013-min.jpg'
     },
     {
       title: 'Радуга',
       description: 'Средняя школа',
       buttonText: 'Увеличить',
-      img: '../../../assets/img/houses/social/house-soc-014.jpg'
+      img: '../../../assets/img/houses/social/house-soc-014-min.jpg'
     },
     {
       title: 'Залесье',
       description: 'Средняя школа',
       buttonText: 'Увеличить',
-      img: '../../../assets/img/houses/social/house-soc-015.jpg'
+      img: '../../../assets/img/houses/social/house-soc-015-min.jpg'
     }
   ];
   slides: any = [[]];
