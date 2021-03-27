96148b4bbb01aa9d924aa5a80ffd919ac13c7ed2
dennis.limontov <dennis.limontov@gmail.com>
2021-03-27 17:45:56 +0300

    start of changing in polyfills and design of contacts

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -32,3 +32,4 @@ import 'zone.js/dist/zone';  // Included with Angular CLI.
 /***************************************************************************************************
  * APPLICATION IMPORTS
  */
+ import '@angular/localize/init';
