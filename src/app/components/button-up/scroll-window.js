769382fd77ca21533e62e4dcec0cabb33a9da2e1
Nick Rimer <nickrimer.03@gmail.com>
2021-04-07 22:19:11 +0300

    -- fix: button up

--- src/app/components/button-up/scroll-window.js
+++ src/app/components/button-up/scroll-window.js
@@ -1,9 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
     document.onscroll = function() {
-      if (window.scrollY > 500) {
-        document.getElementById("button-up").style.display = "initial";
-      } else {
-        document.getElementById("button-up").style.display = "none";
+      const buttonUp = document.getElementById("button-up");
+
+      if (buttonUp) {
+        if (window.scrollY > 500) {
+          buttonUp.style.display = "initial";
+        } else {
+          buttonUp.style.display = "none";
+        }
       }
     }
 });
