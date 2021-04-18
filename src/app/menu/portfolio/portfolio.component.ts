64de2601f8eac0c431d8db83a2414a9df5b5dd34
dennis.limontov <dennis.limontov@gmail.com>
2021-04-19 00:54:51 +0300

    header: changed some styles; added smartphoto folders

--- src/app/menu/portfolio/portfolio.component.ts
+++ src/app/menu/portfolio/portfolio.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from "@angular/core";
+import './smartphoto-1';
 
 @Component({
   selector: "app-portfolio",
   templateUrl: "./portfolio.component.html",
-  styleUrls: ["./portfolio.component.css"],
+  styleUrls: ["./portfolio.component.css", "../../../../node_modules/smartphoto/css/smartphoto.css"],
 })
 export class PortfolioComponent implements OnInit {
   constructor() {}
